766feb6f84941e5823789b233b9f4e52f24dc142
Leela Abhishek sandhya <58944283+abhihemarock@users.noreply.github.com>
2021-01-29 18:02:01 +0530

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,29 @@
+# method 1
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+# method 2
+a,b=11,12
+a,b=b,a
+print(a)
+print(b)
+# method 3
+a=20
+b=30
+a=a^b
+b=a^b
+a=a^b
+print(a)
+print(b)
+# method 4
+a=15
+b=30
+a=a*b
+b=a/b
+a=a/b
+print(int(a))
+print(int(b))

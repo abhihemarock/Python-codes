046fe5437f5a9106f091eaf174502db42021c046
Leela Abhishek sandhya <58944283+abhihemarock@users.noreply.github.com>
2021-01-29 18:01:25 +0530

    Add files via upload

---
+++ NoIsPowOfTwo.py
@@ -0,0 +1,7 @@
+a=int(input())
+if(a&a-1==0):
+    print(a,"yes the given no is power of 2",sep=",")
+else:
+     print(a,"no the given no is not power of 2",sep=",")
+      
+      
---
+++ PosNegZer.py
@@ -0,0 +1,7 @@
+a=int(input())
+if(a>0):
+    print("it is a positive no")
+elif(a<0):
+    print("it is a negetive no")
+else:
+    print("it is equal to zero")

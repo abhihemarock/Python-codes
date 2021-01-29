2b08ebeb3774ab2014f92849d4f0bdccb3b6dcb9
Leela Abhishek sandhya <58944283+abhihemarock@users.noreply.github.com>
2021-01-29 17:59:37 +0530

    Add files via upload

---
+++ LargestOfThree.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if(a>b and a>c):
+    print(a," a is first largest no")
+elif(b>a and b>c):
+     print(b," b is first largest no")
+else:
+     print(c,"c is first largest no")
+ 

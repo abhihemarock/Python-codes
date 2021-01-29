8215bcffe67ce8dfffe1123df1b54789a684c99d
Leela Abhishek sandhya <58944283+abhihemarock@users.noreply.github.com>
2021-01-29 17:59:10 +0530

    Add files via upload

---
+++ Add.py
@@ -0,0 +1,35 @@
+#Add
+a=10
+b=20
+c=a+b
+print(c);
+#int and int
+a=int(input())
+b=int(input())
+c=a+b
+print(c);
+#int and float
+a=int(input())
+b=float(input())
+c=a+b
+print(c);
+#float and float
+a=float(input())
+b=float(input())
+c=a+b
+print(c);
+#int and complex
+a=int(input())
+b=complex(input())
+c=a+b
+print(c);//(11+0j)
+#float and complex
+a=float(input())
+b=complex(input())
+c=a+b
+print(c);//(11+0j)
+#complex and complex
+a=complex(input())
+b=complex(input())
+c=a+b
+print(c);
